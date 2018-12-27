2a805c0573985c1835ba86b55daf4c73709e5137
matt-duell <mkduell@gmail.com>
2018-12-27 09:23:00 -0500

    Updating to allow PUT - removing POST

--- recipe/index.js
+++ recipe/index.js
@@ -3,34 +3,51 @@ module.exports = async function (context, req) {
 
 		context.log(`Request method type: [${context.req.method}]`);
 
-		const recipes = [
-			{
-					"id": 1,
-					"name": "Shreddies",
-					"description": "test1",
-					"categories": [{"id": 1,"name": "Breakfast"}]
-			},
-			{
-					"id": 17,
-					"name": "Recipe 999",
-					"description": "",
-					"categories": [
+		switch (context.req.method){
+			case 'GET':
+				doGetRecipes(context);
+				break;
+			case 'PUT':
+				doAddNewRecipe(context);
+				break;
+		}
+		
+};
+
+function doGetRecipes(context){
+	context.log('Retrieving list of recipes');
+	const recipes = [
+		{
+				"id": 1,
+				"name": "Shreddies",
+				"description": "test1",
+				"categories": [{"id": 1,"name": "Breakfast"}]
+		},
+		{
+				"id": 17,
+				"name": "Recipe 999",
+				"description": "",
+				"categories": [
+					{"id": 1,"name": "Breakfast"},
+					{"id": 2,"name": "dinnerYo"}
+				]
+		},
+		{
+				"id": 18,
+				"name": "Recipe 999",
+				"description": "Recipe 999 description",
+				"categories": [
 						{"id": 1,"name": "Breakfast"},
 						{"id": 2,"name": "dinnerYo"}
-					]
-			},
-			{
-					"id": 18,
-					"name": "Recipe 999",
-					"description": "Recipe 999 description",
-					"categories": [
-							{"id": 1,"name": "Breakfast"},
-							{"id": 2,"name": "dinnerYo"}
-					]
-			}
-		];
-		context.res = {
-			status: 200,
-			body: recipes
-		};
-};
+				]
+		}
+	];
+	context.res = {
+		status: 200,
+		body: recipes
+	};
+}
+
+function doAddNewRecipe(context){
+	context.log('Adding new recipe');
+}
