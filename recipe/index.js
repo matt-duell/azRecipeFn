0af27dd805422603b3846c58bc5e294478f4f253
matt-duell <mkduell@gmail.com>
2018-12-27 21:44:15 -0500

    connecting to db

--- recipe/index.js
+++ recipe/index.js
@@ -1,56 +1,82 @@
+const mongodb = require('mongodb');
+
 module.exports = async function (context, req) {
     context.log('JavaScript HTTP trigger function processed a request.');
-		// DB_CONN_STR
-		context.log(`DB_CONN_STR=[${getEnvVariable("DB_CONN_STR")}]`);
 		context.log(`Request method type: [${context.req.method}]`);
-
+		
 		switch (context.req.method){
 			case 'GET':
 				doGetRecipes(context);
 				break;
-			case 'PUT':
+				case 'PUT':
 				doAddNewRecipe(context);
 				break;
-		}
+			}
+			
+		};
 		
-};
-
-function getEnvVariable(name){
-	return process.env[name];
+		function getEnvVariable(name){
+			return process.env[name];
 }
 
 function doGetRecipes(context){
 	context.log('Retrieving list of recipes');
-	const recipes = [
-		{
-				"id": 1,
-				"name": "Shreddies",
-				"description": "test1",
-				"categories": [{"id": 1,"name": "Breakfast"}]
-		},
-		{
-				"id": 17,
-				"name": "Recipe 999",
-				"description": "",
-				"categories": [
-					{"id": 1,"name": "Breakfast"},
-					{"id": 2,"name": "dinnerYo"}
-				]
-		},
-		{
-				"id": 18,
-				"name": "Recipe 999",
-				"description": "Recipe 999 description",
-				"categories": [
-						{"id": 1,"name": "Breakfast"},
-						{"id": 2,"name": "dinnerYo"}
-				]
+	
+	const DB_CONN_STR_KEY="DB_CONN_STR";
+	context.log(`DB_CONN_STR=[${getEnvVariable(DB_CONN_STR_KEY)}]`);
+	const db_conn_uri = getEnvVariable(DB_CONN_STR_KEY);
+
+	mongodb.MongoClient.connect(db_conn_uri,function(error,client){
+		if(error){
+			context.log('Failed to connect');
+			context.res = { status:500, body: error.stack};
 		}
-	];
-	context.res = {
-		status: 200,
-		body: recipes
-	};
+		context.log('Connected!');
+
+		client.db('recipes').collection('recipes').find().toArray(function(error,docs){
+			if(error){
+				context.log('Error running query');
+				context.res = { status:500, body: error.stack};
+			}
+			context.log('Success!');
+			context.res = {
+				headers: { 'Content-Type': 'application/json'},
+				body: JSON.stringify({res: docs})
+			};
+		});
+	});
+
+
+	// const recipes = [
+	// 	{
+	// 			"id": 1,
+	// 			"name": "Shreddies",
+	// 			"description": "test1",
+	// 			"categories": [{"id": 1,"name": "Breakfast"}]
+	// 	},
+	// 	{
+	// 			"id": 17,
+	// 			"name": "Recipe 999",
+	// 			"description": "",
+	// 			"categories": [
+	// 				{"id": 1,"name": "Breakfast"},
+	// 				{"id": 2,"name": "dinnerYo"}
+	// 			]
+	// 	},
+	// 	{
+	// 			"id": 18,
+	// 			"name": "Recipe 999",
+	// 			"description": "Recipe 999 description",
+	// 			"categories": [
+	// 					{"id": 1,"name": "Breakfast"},
+	// 					{"id": 2,"name": "dinnerYo"}
+	// 			]
+	// 	}
+	// ];
+	// context.res = {
+	// 	status: 200,
+	// 	body: recipes
+	// };
 }
 
 function doAddNewRecipe(context){
